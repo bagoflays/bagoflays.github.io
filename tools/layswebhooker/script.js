4c5966d3b97977d5d74da6668c75ecb9bc4a608f
BagOfLays <108637656+bagoflays@users.noreply.github.com>
2024-12-04 07:54:43 -0800

    Update script.js

--- tools/layswebhooker/script.js
+++ tools/layswebhooker/script.js
@@ -1,3 +1,13 @@
+console.log(navigator.userAgent.toLowerCase())
+if (/android|iphone|ipad/i.test(navigator.userAgent.toLowerCase())) {
+   document.getElementById("stupidtitle").innerText = "Lays Webhooker for Discord for Mobile"
+} else if (/windows/i.test(navigator.userAgent.toLowerCase())) {
+    document.getElementById("stupidtitle").innerText = "Lays Webhooker for Discord for Windows"
+} else if (/macintosh|mac os/i.test(navigator.userAgent.toLowerCase())) {
+    document.getElementById("stupidtitle").innerText = "Lays Webhooker for Discord for MacOS"
+} else if (/linux/i.test(navigator.userAgent.toLowerCase())) {
+   document.getElementById("stupidtitle").innerText = "Lays Webhooker for Discord for Linux/Mobile Desktop Mode"
+}
 function jsSend() {
   const url = prompt('add a webhook url')
   const message = prompt('add a message')

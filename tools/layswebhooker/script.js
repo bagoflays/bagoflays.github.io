9677847514582ea4af4a1ce0625e97d5e2d68523
BagOfLays <108637656+bagoflays@users.noreply.github.com>
2024-12-04 07:50:52 -0800

    Update script.js

--- tools/layswebhooker/script.js
+++ tools/layswebhooker/script.js
@@ -1,3 +1,5 @@
-const url = prompt('add a webhook url')
-const message = prompt('add a message')
-fetch(url,{method: 'POST',headers:{'Content-Type': 'application/json'},body: JSON.stringify({content: message})})
+function jsSend() {
+  const url = prompt('add a webhook url')
+  const message = prompt('add a message')
+  fetch(url,{method: 'POST',headers:{'Content-Type': 'application/json'},body: JSON.stringify({content: message})})
+} 
